e0d37c871aa6f7c60135b555f5ec83f575a8ff53
Afzal Ansari <theafzalansari@gmail.com>
2026-07-25 18:42:03 +0530

    Feat: Particles Background

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -1,5 +1,4 @@
 import {useEffect, useRef} from "react";
-import {color} from "framer-motion";
 
 
 export default function ParticlesBackground(){
@@ -13,7 +12,7 @@ export default function ParticlesBackground(){
         const colors=['rgba(255,255,255,0.7)']
 
         class particle{
-            construtor(){
+            constructor(){
                 this.x = Math.random() * canvas.width;
                 this.y = Math.random() * canvas.height;
                 this.radius = Math.random() * 2+1;
@@ -21,8 +20,56 @@ export default function ParticlesBackground(){
                 this.speedX = (Math.random() - 0.5)*0.5;
                 this.speedY = (Math.random() - 0.5)*0.5;
             }
+            draw(){
+                ctx.beginPath();
+                ctx.arc(this.x , this.y , this.radius , 0 , Math.PI*2);
+                ctx.shadowBlur = 10;
+                ctx.shadowColor = this.color;
+                ctx.fillStyle = this.color;
+                ctx.fill();
+            }
+            update(){
+                this.x += this.speedX;
+                this.y += this.speedY;
+
+                if(this.x < 0) this.x = canvas.width;
+                if(this.x> canvas.width) this.x = 0;
+
+                if(this.y < 0) this.y = canvas.height;
+                if(this.y> canvas.height) this.y = 0;
+
+                this.draw();
+
+            }
         }
-    })
+
+        function createParticles(){
+            particles = [];
+            for(let i = 0; i < particlesCount; i++){
+                particles.push(new particle());
+            }
+        }
+        function handleResize(){
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            createParticles();
+        }
+        handleResize();
+        window.addEventListener("resize", handleResize);
+
+        let animationId;
+        function animate(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            particles.forEach((p) => p.update());
+            animationId = requestAnimationFrame(animate);
+        }
+        animate();
+
+        return () => {
+            cancelAnimationFrame(animationId);
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
 
     return(
         <canvas  ref={canvasRef}
