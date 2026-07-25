5b965a251d7984d90645a5734925169b6aa62e13
Afzal Ansari <theafzalansari@gmail.com>
2026-07-25 22:30:42 +0530

    Feat: Overlay Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import OverlayMenu from "./OverlayMenu.jsx";
+import {useState} from "react";
+import Logo from "../assets/Logo.png"
+import { FiMenu } from "react-icons/fi";
 
-const Navbar = () => {
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [visible, setVisible] = useState(true);
+
     return (
-        <div>Navbar</div>
+        <>
+        <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}>
+
+            <div className="flex items-center space-x-2">
+                <img src={Logo} alt="logo" className="w-8 h-8 "/>
+                <div className="text-2xl font-bold text-white sm:block">Afzal Ansari</div>
+
+            </div>
+            <div className="block lg:absolute lg:left-1/2 lg:transform lg:translate-x-1/2">
+            <button onClick={() => setMenuOpen(true)}
+            className="text-white text-3xl focus:outline-none" aria-label="open Menu"
+            >
+                <FiMenu />
+            </button>
+            </div>
+
+            <div className="hidden lg:block">
+                <a href="#contact"
+                className="bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-100"
+                >
+                    Reach Out
+                </a>
+            </div>
+
+        </nav>
+
+
+        <OverlayMenu isOpen = {menuOpen} onClose = {() => setMenuOpen(false)} />
+        </>
     )
 }
-export default Navbar
